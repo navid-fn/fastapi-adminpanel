2b665b78b6c45b77f2625e3045fb8ac14364b06f
navid <navid.fida1@gmail.com>
2024-04-02 16:48:54 +0330

    mv: all file to app folder

--- app/api/endpoints/posts.py
+++ app/api/endpoints/posts.py
@@ -5,31 +5,33 @@ from sqlmodel import Session, select
 from db.into_db import get_session
 from models.users import User
 
-
 router = APIRouter()
 
 
-@router.get('/', response_model=List[PostGet])
-def get_posts(session: Session = Depends(get_session), offset: int = 0, limit: int = 100):
+@router.get("/", response_model=List[PostGet])
+def get_posts(
+    session: Session = Depends(get_session), offset: int = 0, limit: int = 100
+):
     statement = select(Post).offset(offset).limit(limit)
     posts = session.exec(statement).all()
     return posts
 
-@router.post('/create', response_model=PostGet)
+
+@router.post("/create", response_model=PostGet)
 def create_post(post: PostCreate, session: Session = Depends(get_session)):
     owner = session.get(User, post.owner_id)
     if not owner:
-        raise HTTPException(status_code=404, detail='ownder is not exists.')
+        raise HTTPException(status_code=404, detail="ownder is not exists.")
     db_post = Post.model_validate(post)
     session.add(db_post)
     session.commit()
     session.refresh(db_post)
     return db_post
 
-@router.get('/{post_id}', response_model=PostGet)
+
+@router.get("/{post_id}", response_model=PostGet)
 def get_single_post(post_id: int, session: Session = Depends(get_session)):
     post = session.get(Post, post_id)
     if not post:
-        raise HTTPException(status_code=404, detail='post not exists.')
+        raise HTTPException(status_code=404, detail="post not exists.")
     return post
-
